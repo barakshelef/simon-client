4d8ee714467182c6f6b068e8a5cfca5bb8c6d1e8
shelef <shelef@guardicore.com>
2020-12-30 19:59:27 +0200

    Add songs to SongStorage

--- app/src/main/java/com/team3316/dbugsimon/SongStorage.java
+++ app/src/main/java/com/team3316/dbugsimon/SongStorage.java
@@ -6,32 +6,36 @@ public class SongStorage {
 
     private SongStorage() {
         songs = new Song[]{
-                new Song(new int[]{  // Mary had a little lamb
-                        R.raw.e4,
-                        R.raw.d4,
-                        R.raw.c4,
-                        R.raw.d4,
-                        R.raw.e4,
-                        R.raw.e4,
-                        R.raw.e4,
-                        R.raw.d4,
-                        R.raw.d4,
-                        R.raw.d4,
-                        R.raw.e4,
-                        R.raw.g4,
-                        R.raw.g4,
-                        R.raw.e4,
-                        R.raw.d4,
-                        R.raw.c4,
-                        R.raw.d4,
-                        R.raw.e4,
-                        R.raw.e4,
-                        R.raw.e4,
-                        R.raw.d4,
-                        R.raw.d4,
-                        R.raw.e4,
-                        R.raw.d4,
+                new Song(new int[]{  // MaozTzur
+                        R.raw.c4, R.raw.g3, R.raw.c4, R.raw.f4, R.raw.e4, R.raw.d4, R.raw.c4,
+                        R.raw.g4, R.raw.g4, R.raw.a4, R.raw.d4, R.raw.e4, R.raw.f4, R.raw.e4,
+                        R.raw.d4, R.raw.c4,
+                        R.raw.g4, R.raw.g4, R.raw.g4, R.raw.a4, R.raw.b4, R.raw.c5, R.raw.g4,
+                        R.raw.c5, R.raw.b4, R.raw.a4, R.raw.g4, R.raw.g4, R.raw.f4, R.raw.e4,
+                        R.raw.f4, R.raw.d4,
+                        R.raw.e4, R.raw.f4, R.raw.g4, R.raw.a4, R.raw.d4, R.raw.e4, R.raw.f4,
+                        R.raw.e4, R.raw.c4, R.raw.a4, R.raw.g4, R.raw.f4, R.raw.e4, R.raw.d4,
                         R.raw.c4
+                }),
+
+                new Song(new int[]{  // HanukaHanuka
+                        R.raw.g4, R.raw.e4, R.raw.g4, R.raw.g4, R.raw.e4, R.raw.g4,
+                        R.raw.e4, R.raw.g4, R.raw.c5, R.raw.b4, R.raw.a4,
+                        R.raw.f4, R.raw.d4, R.raw.f4, R.raw.f4, R.raw.d4, R.raw.f4,
+                        R.raw.d4, R.raw.f4, R.raw.b4, R.raw.a4, R.raw.g4,
+                        R.raw.g4, R.raw.e4, R.raw.g4, R.raw.g4, R.raw.e4, R.raw.g4,
+                        R.raw.e4, R.raw.g4, R.raw.c5, R.raw.b4, R.raw.a4,
+                        R.raw.b4, R.raw.b4, R.raw.b4, R.raw.b4, R.raw.b4, R.raw.b4,
+                        R.raw.b4, R.raw.g4, R.raw.a4, R.raw.b4, R.raw.c5
+                }),
+
+                new Song(new int[]{  // BanuHohehLgaresh
+                        R.raw.a4, R.raw.g4, R.raw.a4, R.raw.c5, R.raw.b4, R.raw.c5, R.raw.a4,
+                        R.raw.a4, R.raw.g4, R.raw.a4, R.raw.c5, R.raw.b4, R.raw.c5, R.raw.a4,
+                        R.raw.d5, R.raw.c5, R.raw.d5, R.raw.c5, R.raw.a4, R.raw.c5, R.raw.d5,
+                        R.raw.d5, R.raw.c5, R.raw.d5, R.raw.c5, R.raw.a4, R.raw.c5, R.raw.d5,
+                        R.raw.e5, R.raw.d5, R.raw.c5, R.raw.a4, R.raw.a4, R.raw.g4, R.raw.a4,
+                        R.raw.e5, R.raw.d5, R.raw.c5, R.raw.e5, R.raw.d5, R.raw.c5, R.raw.a4
                 })
         };
     }
